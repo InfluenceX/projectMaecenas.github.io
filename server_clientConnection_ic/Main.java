67627477ed40cd9eb8c44e580c291b998b900b11
Claire Adriani <claireadriani@MBP-di-Claire.homenet.telecomitalia.it>
2017-12-31 09:51:27 +0100

    s

--- server_clientConnection_ic/Main.java
+++
@@ -1,9 +0,0 @@
-package myWork;
-
-public class Main {
-    public static void main(String argvs[]) {
-        int PORT=4444;
-        Thread server=new Thread(new serverSocketOpen(PORT));
-        server.start();
-    }
-}
